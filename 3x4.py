e367a7b0c0627a644c4bac6e1d375b9596b61e71
Richard Homa <homa@warren.edu>
2018-11-17 14:31:35 -0500

    Minor changes

--- 3x4.py
+++ 3x4.py
@@ -6,7 +6,7 @@ print(z)
 teams = ["Pirates","Phillies","Expos", "Mets", "Cubs", "Cardinals"]
 for i in teams:
     print(i)
-print("Hello Git")    
+print("Hello Git")   
    
 #new comment from GitHub
 #new comment from GitHub Windows Version
