64b514ae80da93e30938bbc271492f3d3e21ea09
Richard Homa <homa@warren.edu>
2018-11-17 14:25:10 -0500

    Adding new py file

---
+++ 3x4.py
@@ -0,0 +1,12 @@
+x=3
+y=4
+z=x*y
+print(z)
+
+teams = ["Pirates","Phillies","Expos", "Mets", "Cubs", "Cardinals"]
+for i in teams:
+    print(i)
+print("Hello Git")    
+   
+#new comment from GitHub
+#new comment from GitHub Windows Version
